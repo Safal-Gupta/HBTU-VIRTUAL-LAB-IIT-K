853c1ec93a316d1bea142a41549e1564b26be999
Safal-Gupta <safalgupta040310@gmail.com>
2024-07-19 16:59:05 +0530

    changes

--- step1.js
+++ step1.js
@@ -1,7 +1,25 @@
+
 var bgn = document.querySelector(".bg");
 var part1 = document.querySelector(".part1");
 var nxt1 = document.querySelector(".nxt1");
 const imc = () => {
     part1.classList.remove("hide");
 };
+const pnc = () => {
+    bgn.classList.add("hide");
+}
+
+
+
+
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+const dotLottie = new DotLottie({
+    autoplay: true,
+    loop: true,
+    canvas: document.querySelector('#dotlottie-canvas'),
+    src: "https://lottie.host/13c44a80-fb02-4277-b17b-e29a88a4dd19/PiwEsljqLo.json",
+});
+
 nxt1.addEventListener("click", imc);
+nxt1.addEventListener("click", pnc);
+
